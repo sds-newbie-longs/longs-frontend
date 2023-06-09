85ce8dcc49e8392d776133540b38293ccca34db3
din.ryu <vidigummy@gmail.com>
2023-06-09 12:23:53 +0900

    [SL-105] feat:Header Component 생성

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () =>{
+    return (
+        <div className='app-header'>
+            <div className='app-header-search-field-wrapper'></div>
+            <div className='app-header-add-content-button-wrapper'></div>
+        </div>
+    )
+}
+
+export default Header;
