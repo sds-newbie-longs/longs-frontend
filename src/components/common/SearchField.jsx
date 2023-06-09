b9e4d14cf14f5a5e5870156d55046fef44f26005
hyeonho9877 <6190606@naver.com>
2023-06-09 13:50:37 +0900

    [SL-84] feat: SearchField 구현

---
+++ src/components/common/SearchField.jsx
@@ -0,0 +1,54 @@
+import React, { useCallback, useRef } from 'react';
+import PropTypes from 'prop-types';
+import SearchSvg from 'components/common/SearchSvg';
+import 'styles/SearchField.scss';
+
+const SearchField = props => {
+  const { isBordered, placeholder, handleSubmit } = props;
+
+  const inputRef = useRef();
+
+  const getContainerClassName = useCallback(() => {
+    if (isBordered) return 'search-field-container-bordered';
+    else return 'search-field-container-borderless';
+  }, []);
+
+  const onSubmit = useCallback(data => {
+    if (data.length !== 0) {
+      handleSubmit(data);
+      inputRef.current.value = '';
+    }
+  }, []);
+
+  const onKeyDown = useCallback(evt => {
+    if (evt.key === 'Enter') {
+      onSubmit(evt.target.value);
+    }
+  }, []);
+
+  const onSearchIconClick = useCallback(() => {
+    onSubmit(inputRef.current.value);
+  }, []);
+
+  return (
+    <div className={'search-field-root'}>
+      <div className={getContainerClassName()}>
+        <input
+          className={'search-field-input'}
+          placeholder={placeholder}
+          onKeyDown={onKeyDown}
+          ref={inputRef}
+        />
+        <SearchSvg className={'search-field-icon'} onClick={onSearchIconClick} />
+      </div>
+    </div>
+  );
+};
+
+export default SearchField;
+
+SearchField.propTypes = {
+  isBordered: PropTypes.bool.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
