ba62cb4d25e313f4ce36986d230aa4eb0b012261
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2023-06-12 13:58:44 +0900

    [SL-87] feat : video info list 구현
    * [NO-ISSUE] chore: .idea 밑에 있는건 필요 없습니다.
    
    * [SL-118] feat ComponentsArea 생성
    
    Mock API를 이용해 ComponentsArea 자료 구조 생성, Props 또한 Mock이다.
    
    * [STASH] 집에 가서 할겁니다.(스타일 진행중)
    
    * [SL-124] feat: css 추가 assets 추가
    
    * [NO-ISSUE] chore : conflict resolve
    
    * [NO-ISSUE] conflict resolve
    
    * [SL-84] style: 이벤트 핸들러 이름 변경
    
    * [NO-ISSUE] fix style EOF 추가
    
    * [NO-ISSUE] chore: github action 배포 pipeline 정립 
    
    배포 가능한~
    
    * [SL-80] feat:그룹사이드바 추가
    
    * [SL-80]feat:그룹사이드바 수정
    
    절대 경로로 변경
    
    * [SL-87] 리스트 만들면서 info도 어느정도는 개발
    
    * [NO-ISSUE] chore : 절대 경로 처리
    
    ---------
    
    Co-authored-by: vidigummy <vidigummy@gamil.com>
    Co-authored-by: hyeonho9877 <6190606@naver.com>
    Co-authored-by: yongjang cho <dydwkd48670@gmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 import 'styles/Header.scss';
-const Header = () =>{
-    return (
-        <div className='app-header'>
-            <div className='app-header-search-field-wrapper'></div>
-            <div className='app-header-add-content-button-wrapper'></div>
-        </div>
-    )
-}
+const Header = () => {
+  return (
+    <div className="app-header">
+      <div className="app-header-search-field-wrapper"></div>
+      <div className="app-header-add-content-button-wrapper"></div>
+    </div>
+  );
+};
 
 export default Header;
--- src/utils/ContentAreaApiModule.js
+++ src/utils/ContentAreaApiModule.js
@@ -2,11 +2,15 @@ function ContentAreaApiModule() {
   this.mockGetAllContentsWithGroup = async group => {
     const content = {};
     content.picture_url =
+      'https://i.namu.wiki/i/BQ-BNOkKyAyDPfGEj5fBDSp11JS2rYioh0qKzl1A9UXmmWz5YKoR-1AwOQyUMb0FdvnpOFNnZ0_S6rj-GEDOuE-mmVCUozWDphHPN_thbViZynmST_2dRLdFXkcphNiQ7YC4CeuteLx_uSxGSBluKA.webp';
+    content.owner = 'din';
+    content.title = 'Want to Go Home';
+    const mockContentsList = [];
+    for (let i = 0; i < 10; i++) {
       'https://namu.wiki/jump/Gc3gjN02gz6766%2FcLetBPH4OVqHRE1oOsI28oM2vjkwbG5IN1xaAJvsjlSzj15ik';
     content.owner = 'din';
     content.title = 'Want to Go Home';
     const mockContentsList = [];
-    for (let i = 0; i < 0; i++) {
       mockContentsList.push(Object.assign({}, content));
     }
     return mockContentsList;
@@ -14,6 +18,11 @@ function ContentAreaApiModule() {
   this.mockGetAllContentsWithName = async (group, name) => {
     const content = {};
     content.picture_url =
+      'https://i.namu.wiki/i/BQ-BNOkKyAyDPfGEj5fBDSp11JS2rYioh0qKzl1A9UXmmWz5YKoR-1AwOQyUMb0FdvnpOFNnZ0_S6rj-GEDOuE-mmVCUozWDphHPN_thbViZynmST_2dRLdFXkcphNiQ7YC4CeuteLx_uSxGSBluKA.webp';
+    content.owner = 'soy';
+    content.title = 'Want to Go Home';
+    const mockContentsList = [];
+    for (let i = 0; i < 10; i++) {
       'https://namu.wiki/jump/Gc3gjN02gz6766%2FcLetBPH4OVqHRE1oOsI28oM2vjkwbG5IN1xaAJvsjlSzj15ik';
     content.owner = 'soy';
     content.title = 'Want to Go Home';
