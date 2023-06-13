db777807d781b7fb64571576f66ed1d2677e6b4b
kims0uce <ksy_59@naver.com>
2023-06-13 13:54:50 +0900

    [SL-54] [SL-55] feat: 코드 전체 업로드

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.scss';
 
+import Login from 'components/Login';
+// import Upload from 'components/Upload';
+// import Dropzone from 'components/Dropzone';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Login />
+      {/* <Upload /> */}
+    </div>
+  );
 }
 
 export default App;
