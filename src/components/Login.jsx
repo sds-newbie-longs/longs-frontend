b52f92cf7c7b5d817cef5ab8410e8bd123a56f17
hyeonho9877 <6190606@naver.com>
2023-06-14 15:58:07 +0900

    [NO-ISSUE] bug: merge issue

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import 'styles/Login.scss';
-import Logo from 'assets/Logo.png';
+import Logo from 'assets/MainLogo.svg';
 import { useNavigate } from 'react-router';
 
 const Login = () => {
