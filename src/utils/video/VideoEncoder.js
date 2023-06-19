1381e8139f41cefcac4d21a394611512309c8052
hyeonho9877 <6190606@naver.com>
2023-06-19 10:20:40 +0900

    [SL-154] feat: 동영상 인코딩 구현

---
+++ src/utils/video/VideoEncoder.js
@@ -0,0 +1,38 @@
+function encode(name, data) {
+  const worker = new Worker(new URL('VideoWorker.js', import.meta.url));
+  worker.onmessage = function (e) {
+    const msg = e.data;
+    switch (msg.type) {
+      case 'ready':
+        console.log('worker ready');
+        worker.postMessage({ type: 'run', arguments: { name, data } });
+        break;
+      case 'stdout':
+        console.log(msg.data);
+        break;
+      case 'stderr':
+        console.log(msg.data);
+        break;
+      case 'start':
+        console.log(msg.data);
+        break;
+      case 'done':
+        console.log(msg.data);
+        break;
+    }
+  };
+}
+
+// 테스트용 메소드
+/* function createDownloadLinkElement(data) {
+  const downloadLink = document.createElement('a');
+  downloadLink.href = URL.createObjectURL(createBlob(data));
+  downloadLink.download = 'encoded.mp4'; // 저장될 파일명 및 확장자
+  document.body.appendChild(downloadLink);
+}
+
+function createBlob(data) {
+  return new Blob([data], { type: 'application/octet-stream' });
+} */
+
+export { encode };
