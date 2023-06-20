271906ed49efdd5ca5a613062e1d3048cbb397fb
kims0uce <ksy_59@naver.com>
2023-06-20 14:36:33 +0900

    [SL-188] feat: imsi

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -1,11 +1,17 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import 'styles/Dropzone.scss';
 import AddButton from 'components/common/AddButton';
 import PropTypes from 'prop-types';
 // import { encode } from 'utils/video/VideoEncoder';
 import TusUploader from 'utils/video/TusUploader';
+
 const Dropzone = props => {
+  const [currentProgress, setCurrentProgress] = useState(0);
+  const done = 'done';
+
   const onDrop = useCallback(acceptedFiles => {
     const file = acceptedFiles[0];
     const endpoint = 'http://localhost:8080/video/upload';
@@ -21,22 +27,35 @@ const Dropzone = props => {
         filetype: file.type,
       });
       const onProgress = (bytesUploaded, bytesTotal) => {
-        const percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2);
+        const percentage = Math.round((bytesUploaded / bytesTotal) * 100);
         console.log(bytesUploaded, bytesTotal, percentage + '%');
+        setCurrentProgress(percentage);
       };
       const onSuccess = () => {
-        console.log('Download %s from %s', file.name);
+        console.log('Download %s from %s', file.name, file.type);
       };
       const onError = err => {
         console.log(err);
       };
       const onBeforeRequest = req => {
-        const xhr = req.getUnderlyingObject();
-        xhr.withCredentials = true;
+        // 로컬 노드js 테스트 시 주석
+        // const xhr = req.getUnderlyingObject();
+        // xhr.withCredentials = true;
       };
-      uploader.startUpload(onProgress, onSuccess, onError, onBeforeRequest);
+      const onAfterResponse = (req, res) => {
+        setCurrentProgress(101);
+        const url = req.getURL();
+        const vid = url.split('/')[4];
+
+        if (vid !== 'upload') {
+          console.log(`videoUUID => ${vid}`);
+        }
+      };
+
+      uploader.startUpload(onProgress, onSuccess, onError, onBeforeRequest, onAfterResponse);
     };
   }, []);
+
   const { getRootProps, getInputProps, open, acceptedFiles } = useDropzone({
     noClick: true,
     accept: {
@@ -44,14 +63,24 @@ const Dropzone = props => {
     },
     onDrop,
   });
+
   const files = acceptedFiles.map(file => <p key={file.path}>{file.path}</p>);
   useEffect(() => {
     if (files.length > 0) {
       props.setIsUpload(true);
     }
   }, [files.length]);
+
   if (files.length > 0) {
-    return <div className={'drop-container-full'}>{files}</div>;
+    return (
+      <div className={'drop-container-full'}>
+        <CircularProgressbar
+          value={currentProgress}
+          text={`${currentProgress < 101 ? currentProgress : done}%`}
+          maxValue={101}
+        />
+      </div>
+    );
   }
   return (
     <div className={'drop-container-empty'}>
