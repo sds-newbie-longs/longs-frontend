81523afa884972950882ae5a0aea908e510e74e2
kims0uce <ksy_59@naver.com>
2023-06-21 16:06:31 +0900

    [SL-188] feat: 누락 파일 재업로드

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -3,11 +3,14 @@ import React, { useState } from 'react';
 import 'styles/Upload.scss';
 import Dropzone from 'components/Dropzone';
 import CloseBtn from 'assets/CloseBtn.png';
+import { useNavigate } from 'react-router';
 
 const Upload = () => {
   const [isUpload, setIsUpload] = useState(false);
   const [title, setTitle] = useState('');
   const [description, setDesscription] = useState('');
+  const [uuid, setUuid] = useState('');
+  const navigator = useNavigate();
 
   const TitleChange = e => {
     setTitle(e.target.value);
@@ -26,17 +29,23 @@ const Upload = () => {
 
   const buttonClassName = e => {
     if (isUpload && title.length > 0) {
+      console.log(uuid);
       return 'button-click';
     }
     return 'button-nor';
   };
 
+  const ClickToGoMain = () => {
+    navigator('/');
+    console.log('go to main...');
+  };
+
   return (
     <div className={'upload-root'}>
       <div className={'upload-container'}>
-        <img className={'close-button'} src={CloseBtn} />
+        <img className={'close-button'} src={CloseBtn} onClick={ClickToGoMain} />
         <p className={'upload-ment'}>Upload Files</p>
-        <Dropzone setIsUpload={setIsUpload} />
+        <Dropzone setIsUpload={setIsUpload} setUuid={setUuid} />
         <div className={'text-container'}>
           <div className={'text-group'}>
             <p className={'group-ment1'}>Group</p>
@@ -60,7 +69,9 @@ const Upload = () => {
             Upload File
           </button>
           <p>or</p>
-          <button className={'cancle-btn'}>cancle</button>
+          <button className={'cancle-btn'} onClick={ClickToGoMain}>
+            cancle
+          </button>
         </div>
       </div>
     </div>
